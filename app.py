328dffafc1bf35413d441ac0182eb17fef3c9771
Ruvina <vasruvina@gmail.com>
2025-03-19 00:07:10 +0530

    Added host and port in app.run

--- app.py
+++ app.py
@@ -37,5 +37,7 @@ def index():
 
     return render_template("index.html", users=users, error=error)
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
